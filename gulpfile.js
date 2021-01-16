a31be9ea69adb7b3e09c5a4133541ddb2485b8e2
Marty Zalega <marty@zalega.me>
2021-01-16 14:03:20 +1000

    Update dependencies and upgrade gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,39 @@
-const gulp = require('gulp')
+const { series, src, dest, watch } = require('gulp')
 const postcss = require('gulp-postcss')
 const cssnext = require('postcss-cssnext')
 const cssnano = require('cssnano')
-const browserSync = require('browser-sync')
-const { reload } = browserSync
+const browserSync = require('browser-sync').create()
+
+const plugins = [
+  cssnext({ features: { rem: false } }),
+  cssnano(),
+]
 
 const srcPath = './src'
 const htmlGlob = srcPath + '/*.html'
 const cssGlob = srcPath + '/*.css'
 const distPath = './dist'
 
-gulp.task('default', [ 'css', 'html' ])
-
-gulp.task('html', () => {
-  return gulp.src(htmlGlob)
-    .pipe(gulp.dest(distPath))
-    .pipe(reload({ stream: true }))
-})
-
-gulp.task('css', () => {
-  const plugins = [
-    cssnext({ features: { rem: false } }),
-    cssnano(),
-  ]
-
-  return gulp.src(cssGlob)
+function css() {
+  return src(cssGlob)
     .pipe(postcss(plugins))
-    .pipe(gulp.dest(distPath))
-    .pipe(reload({ stream: true }))
-})
+    .pipe(dest(distPath))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('serve', [ 'html', 'css' ], () => {
-  const server = { baseDir: distPath }
-  browserSync({ server })
+function html() {
+  return src(htmlGlob)
+    .pipe(dest(distPath))
+}
 
-  gulp.watch(htmlGlob, [ 'html' ])
-  gulp.watch(cssGlob, [ 'css' ])
-})
+function serve() {
+  browserSync.init({ server: distPath })
+
+  watch(htmlGlob, html).on('change', browserSync.reload)
+  watch(cssGlob, css)
+}
+
+exports.css = css
+exports.html = html
+exports.serve = series(css, html, serve)
+exports.default = series(css, html)
